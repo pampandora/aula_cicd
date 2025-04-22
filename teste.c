065c2e84cb3684ab7e4bf0cbda110a03085b592b
pampandora <168042587+pampandora@users.noreply.github.com>
2025-04-22 19:42:10 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <assert.h>
+
+void testar(){
+  //função de teste simples
+  assert(1 == 1); //verifica se 1 é igual a 1
+  printf("teste passou\n")
+}
+
+int main(){
+  testar();
+  return 0;
+}
